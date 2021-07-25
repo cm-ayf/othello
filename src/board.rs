f0704a27ac9143743fb25b27ee8193d0bcc05585
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-07-26 01:13:00 +0900

    impl board.put()

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Board {
     state: [[usize; 8]; 8],
     next: usize,
@@ -19,5 +19,81 @@ impl Board {
             [0; 8]
         ] as [[usize; 8]; 8];
         Board { state, next: 1 }
+    }
+
+    pub fn put(&mut self, row_pos: usize, col_pos: usize) -> Result<(),String> {
+        if !((0..8).contains(&row_pos) && (0..8).contains(&col_pos)) {
+            return Err(String::from("argument(s) out of bound"));
+        }
+        if self.check(row_pos, col_pos) > 0 {
+            self.reverse(row_pos, col_pos);
+        } else {
+            return Err(String::from("no reverses"));
+        }
+        self.next = 3 - self.next;
+        for i in 0..8 {
+            for j in 0..8 {
+                if self.state[i][j] > 2 {
+                    self.state[i][j] = 0;
+                }
+                if self.state[i][j] == 0 && self.check(i, j) > 0 {
+                    self.state[i][j] = self.next + 2;
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn check(&self, row_pos: usize, col_pos: usize) -> usize {
+        if let 1 | 2 = self.state[row_pos][col_pos] {
+            return 0;
+        }
+        let cnt = [(2,1),(2,2),(1,2),(0,2),(0,1),(0,0),(1,0),(2,0)].iter().map(|(di, dj)| {
+            let mut cnt: usize = 0;
+            let mut i = row_pos;
+            let mut j = col_pos;
+            loop {
+                if i + di < 1 || i + di > 8 || j + dj < 1 || j + dj > 8 {
+                    return 0;
+                }
+                i += di;
+                i -= 1;
+                j += dj;
+                j -= 1;
+                if let 0 | 3 | 4 = self.state[i][j] {
+                    return 0;
+                }
+                if self.state[i][j] == self.next {
+                    break;
+                }
+                cnt += 1;
+            }
+            cnt
+        }).sum();
+        cnt
+    }
+
+    fn reverse(&mut self, row_pos: usize, col_pos: usize) {
+        self.state[row_pos][col_pos] = self.next;
+        [(2,1),(2,2),(1,2),(0,2),(0,1),(0,0),(1,0),(2,0)].iter().for_each(|(di, dj)| {
+            let mut i = row_pos;
+            let mut j = col_pos;
+            loop {
+                if i + di < 1 || i + di > 8 || j + dj < 1 || j + dj > 8 {
+                    return;
+                }
+                i += di; 
+                i -= 1;
+                j += dj; 
+                j -= 1;
+                if let 0 | 3 | 4 = self.state[i][j] {
+                    return;
+                }
+                if self.state[i][j] == self.next {
+                    return;
+                }
+                self.state[i][j] = self.next;
+            }
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use futures::{SinkExt, StreamExt, channel::mpsc};
+use futures::{SinkExt, StreamExt, channel::mpsc, lock::Mutex};
 use warp::{Filter, fs, ws::*};
 
 mod board;
@@ -9,10 +9,10 @@ async fn main() {
     std::env::set_var("RUST_LOG", "server");
     env_logger::init();
 
-    let file 
+    let file
         = fs::dir("resource");
-        
-    let ws 
+
+    let ws
         = warp::path("ws")
         .and(warp::ws())
         .map(|ws: Ws| {
@@ -20,20 +20,47 @@ async fn main() {
                 let (ws_tx, ws_rx) = websocket.split();
                 let (fc_tx, fc_rx) = mpsc::unbounded();
                 let forward = fc_rx.forward(ws_tx);
-                let board = Board::new();
+                let board = Mutex::new(Board::new());
                 let handler = ws_rx.for_each(|msg| async {
+                    let mut board = board.lock().await;
                     let str = String::from(msg.unwrap().to_str().unwrap());
-                    if str.contains("ping") {
-                        fc_tx.clone().send(Ok(Message::text("pong"))).await.unwrap();
-                    }
-                    if str.contains("DOMContentLoaded") {
-                        let json = serde_json::to_string(&board).unwrap();
-                        fc_tx.clone().send(Ok(Message::text(json))).await.unwrap();
+                    let mut cmd = str.split_whitespace();
+                    match cmd.next() {
+                        Some("ping") => fc_tx.clone().send(Ok(Message::text("pong"))).await.unwrap(),
+                        Some("DOMContentLoaded") => {
+                            let json = serde_json::to_string(&*board).unwrap();
+                            fc_tx.clone().send(Ok(Message::text(json))).await.unwrap();
+                        },
+                        Some("put") => {
+                            let args = cmd.collect::<Vec<_>>();
+                            if args.len() < 2 {
+                                fc_tx.clone().send(Ok(Message::text("put: not enough arguments"))).await.unwrap();
+                            } else {
+                                match (args[0].parse::<usize>(), args[1].parse::<usize>()) {
+                                    (Ok(row_pos), Ok(col_pos)) => {
+                                        match board.put(row_pos, col_pos) {
+                                            Ok(_) => {
+                                                let json = serde_json::to_string(&*board).unwrap();
+                                                fc_tx.clone().send(Ok(Message::text(json))).await.unwrap();
+                                            },
+                                            Err(e) => fc_tx.clone().send(Ok(Message::text(e))).await.unwrap(),
+                                        }
+                                    },
+                                    _ => {
+                                        fc_tx.clone().send(Ok(Message::text("put: invalid argument: must be usize"))).await.unwrap();
+                                    }
+                                }
+                                
+                            }
+                            
+                        }
+                        Some(str) => fc_tx.clone().send(Ok(Message::text(format!("invalid command {}", str)))).await.unwrap(),
+                        None => fc_tx.clone().send(Ok(Message::text("no command parsed"))).await.unwrap(),
                     }
                 });
                 futures::pin_mut!(forward, handler);
                 futures::future::select(forward, handler).await;
-                
+
             })
         });
     warp::serve(ws.or(file).with(warp::log("server"))).run(([127, 0, 0, 1], 3000)).await;

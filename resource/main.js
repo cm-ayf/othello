08fab018088ab9643669591e18bedb83b7e330e5
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-07-25 16:43:56 +0900

    ping-pong

--- resource/main.js
+++ resource/main.js
@@ -2,8 +2,8 @@ const ws = new WebSocket(`ws://${location.host}/ws`);
 
 ws.addEventListener('open', (e) => console.log(e));
 
-ws.addEventListener('message', (e) => console.log(e));
+ws.addEventListener('message', (e) => console.log(e.data));
 
 document.addEventListener('DOMContentLoaded',function(e){
-    document.getElementById('sample').addEventListener('click', (e) => ws.send(e.type));
+    document.getElementById('sample').addEventListener('click', (e) => ws.send('ping'));
 });
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use futures::{FutureExt, StreamExt};
+use futures::{SinkExt, StreamExt, channel::mpsc};
 use warp::{Filter, fs, ws};
 
 #[tokio::main]
@@ -6,7 +6,6 @@ async fn main() {
     std::env::set_var("RUST_LOG", "server");
     env_logger::init();
 
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
     let file 
         = fs::dir("resource");
         
@@ -14,13 +13,18 @@ async fn main() {
         = warp::path("ws")
         .and(warp::ws())
         .map(|ws: ws::Ws| {
-            ws.on_upgrade(|websocket| {
-                let (tx, rx) = websocket.split();
-                rx.forward(tx).map(|result| {
-                    if let Err(e) = result {
-                        eprintln!("websocket error: {:?}", e);
+            ws.on_upgrade(|websocket| async {
+                let (ws_tx, ws_rx) = websocket.split();
+                let (fc_tx, fc_rx) = mpsc::unbounded();
+                let forward = fc_rx.forward(ws_tx);
+                let ping_pong = ws_rx.for_each(|msg| async {
+                    if msg.unwrap().to_str().unwrap().contains("ping") {
+                        fc_tx.clone().send(Ok(ws::Message::text("pong"))).await.unwrap();
                     }
-                })
+                });
+                futures::pin_mut!(forward, ping_pong);
+                futures::future::select(forward, ping_pong).await;
+                
             })
         });
     warp::serve(ws.or(file).with(warp::log("server"))).run(([127, 0, 0, 1], 3000)).await;

a5113190d1d9712eed9c6bde2239008d7ff81888
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-07-25 15:52:02 +0900

    impl serverside websocket

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use warp::{Filter, fs};
+use futures::{FutureExt, StreamExt};
+use warp::{Filter, fs, ws};
 
 #[tokio::main]
 async fn main() {
@@ -7,7 +8,20 @@ async fn main() {
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let file 
-        = fs::dir("resource")
-        .with(warp::log("server"));
-    warp::serve(file).run(([127, 0, 0, 1], 3000)).await;
+        = fs::dir("resource");
+        
+    let ws 
+        = warp::path("ws")
+        .and(warp::ws())
+        .map(|ws: ws::Ws| {
+            ws.on_upgrade(|websocket| {
+                let (tx, rx) = websocket.split();
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket error: {:?}", e);
+                    }
+                })
+            })
+        });
+    warp::serve(ws.or(file).with(warp::log("server"))).run(([127, 0, 0, 1], 3000)).await;
 }

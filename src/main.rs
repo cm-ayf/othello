d09abb786e95f74c44af1d1a6012dff949049c20
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-07-25 15:31:56 +0900

    return resources in resource

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use warp::{Filter, fs};
+
+#[tokio::main]
+async fn main() {
+    std::env::set_var("RUST_LOG", "server");
+    env_logger::init();
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let file 
+        = fs::dir("resource")
+        .with(warp::log("server"));
+    warp::serve(file).run(([127, 0, 0, 1], 3000)).await;
 }

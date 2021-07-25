6fa9fe6243f37c4a6cd078bf876a1af8edbaa9bb
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-07-25 16:07:00 +0900

    impl clientside websocket

--- resource/main.js
+++ resource/main.js
@@ -1,1 +1,9 @@
-console.log('hello world!');
+const ws = new WebSocket(`ws://${location.host}/ws`);
+
+ws.addEventListener('open', (e) => console.log(e));
+
+ws.addEventListener('message', (e) => console.log(e));
+
+document.addEventListener('DOMContentLoaded',function(e){
+    document.getElementById('sample').addEventListener('click', (e) => ws.send(e.type));
+});
